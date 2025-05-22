40cecdc8bfa6d3e8c3ad33f69f2ecdc1309af41d
mstfsu <mustafasu363636@hotmail.com>
2025-05-22 15:49:14 +0300

    Update setup.py to enhance project metadata and dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="query_summary",
-    version="0.1.0",
-    description="A Flask middleware to track MongoDB queries and display summaries.",
+    name="query_summary",  # Package name
+    version="0.1.0",  # Initial version
+    description="A Flask middleware to track MongoDB queries and provide query statistics.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="mstfsu",
     author_email="su.mustafa@hotmail.com",
+    url="https://github.com/mstfsu/query_summary",  # Main project URL (GitHub repo)
+    project_urls={  # Additional URLs
+        "Bug Tracker": "https://github.com/mstfsu/query_summary/issues",
+        "Documentation": "https://github.com/mstfsu/query_summary#readme",
+        "Source Code": "https://github.com/mstfsu/query_summary",
+    },
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
+        "Flask>=2.0",
         "pymongo>=4.0",
-        "flask>=2.0",
         "mongoengine>=0.24.0",
     ],
     classifiers=[
@@ -19,4 +26,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    include_package_data=True,
 )
